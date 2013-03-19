67d62778b06a76d3b92fc10db5d1db05b2adb5fb
Robin Chou <chourobin@gmail.com>
2013-03-19 13:21:26 -0400

    Initial Commit

---
+++ RPGExample/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  RPGExample
+//
+//  Created by Robin Chou on 3/19/13.
+//  Copyright (c) 2013 Robin Chou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
