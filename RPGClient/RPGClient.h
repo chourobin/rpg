f451d11595a6bc33487dcb706c5e7f34a530fa1f
Robin Chou <chourobin@gmail.com>
2013-03-19 13:22:04 -0400

    initial commit

---
+++ RPGClient/RPGClient.h
@@ -0,0 +1,13 @@
+//
+//  RPGClient.h
+//  RPGClient
+//
+//  Created by Robin Chou on 3/19/13.
+//  Copyright (c) 2013 Robin Chou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RPGClient : NSObject
+
+@end
---
+++ RPGClient/RPGClient.m
@@ -0,0 +1,13 @@
+//
+//  RPGClient.m
+//  RPGClient
+//
+//  Created by Robin Chou on 3/19/13.
+//  Copyright (c) 2013 Robin Chou. All rights reserved.
+//
+
+#import "RPGClient.h"
+
+@implementation RPGClient
+
+@end
